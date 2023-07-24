3b6a6029f04c98e65e75051238e30e6422c4ef38
xizhang <xizhang4949011@outlook.com>
2023-07-24 08:51:53 -0700

    change url

--- BookSale/src/app/shared/environments/environments.ts
+++ BookSale/src/app/shared/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environments={
-  // api : 'http://ec2-54-202-147-40.us-west-2.compute.amazonaws.com:8083'
-  api : 'http://localhost:8083'
+  api : 'http://ec2-54-202-147-40.us-west-2.compute.amazonaws.com:8083'
+  // api : 'http://localhost:8083'
 };
